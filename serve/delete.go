1a84d8e516474c5153b65d1e231f3354a3b1f284
Jeff Heidel <jheidel@gmail.com>
2018-02-10 19:30:56 -0800

    Add missing delete server implementation.

---
+++ serve/delete.go
@@ -0,0 +1,33 @@
+package serve
+
+import (
+	"cam/video"
+	"fmt"
+	"net/http"
+)
+
+type DeleteServer struct {
+	FS *video.Filesystem
+}
+
+func (s *DeleteServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := r.Form.Get("id")
+	vr := s.FS.GetRecordByID(id)
+	if vr == nil {
+		http.Error(w, fmt.Sprintf("No record found for id %v", id), http.StatusNotFound)
+		return
+	}
+
+	vr.Delete()
+	s.FS.Refresh()
+}
